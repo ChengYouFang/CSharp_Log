5bf77f296550a65e43434b2211741e7f99bf25dc
ChengYou Fang <s47548081@hotmail.com>
2013-03-18 00:34:47 +0800

    Beta v0.0.1

---
+++ Log/Log.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace CYFang.Log
+{
+    public static class Log
+    {
+        public static string FilePath { set; get; }
+
+        public static void writeLog(string message)
+        {
+            if (string.IsNullOrEmpty(FilePath))
+            {
+                FilePath = Directory.GetCurrentDirectory();
+            }
+
+            string filename = FilePath + string.Format("\\{0:yyyy}\\{0:MM}\\{0:yyyy-MM-dd}.txt", DateTime.Now);
+            FileInfo info = new FileInfo(filename);
+            if (info.Directory.Exists == false)
+            {
+                info.Directory.Create();
+            }
+            string log = message + Environment.NewLine;
+            File.AppendAllText(FilePath, log);
+        }
+    }
+}
